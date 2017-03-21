8fe17ceaf720e79a06489017dbb42add380189a8
Etienne <etienne1296pro@gmail.com>
2017-03-21 21:27:12 +0100

    Ajout des fonctions js et d'autres éléments (non fonctionnel)

--- app/scripts/files/errMsg/errMsg.js
+++ app/scripts/files/errMsg/errMsg.js
@@ -4,4 +4,61 @@
  * CREATEUR : ETIENNE BLANC-COQUAND (etienne1296pro@gmail.com)
  * FONCTIONS : RETOURNER DES MESSAGES D'ERREUR SI LA SAISIE N'EST PAS CORRECTE
  */
-/* eslint-env browser */
+$(document).ready(function () {
+    var $nom = $("#name_field"),
+        $prenom = $("#surname_field"),
+        $numTel = $("#phoneNbr_field"),
+        $mail = $("#mail_field"),
+        $genderMale = $("#gender_field_male"),
+        $genderFemale = $("#gender_field_female"),
+        $country = $("#country"),
+        $submit = $("#submit"),
+        $reset = $("#reset"),
+        $errMsg = $("#errorMessage"),
+        $champ = $(".champ");
+
+    $numTel.keyup(function () {
+        if($(this).val().length < 10 || $(this).val().length > 10){
+            $(this).css({
+                borderColor : 'red',
+                color: 'red'
+            })
+        }else{
+            $(this).css({
+                borderColor : 'green',
+                color: 'green'
+            })
+        }
+    });
+
+    $submit.click(function (e) {
+        e.preventDefault(); //Annuler la fonction par défaut du bouton d'envoi
+
+        verifier($nom);
+        verifier($prenom);
+        verifier($numTel);
+        verifier($mail);
+    });
+
+    $reset.click(function () {
+        $champ.css({
+            borderColor : 'grey',
+            color : 'color'
+        });
+        $errMsg.css('display', 'none');
+    });
+
+    function verifier(champ) {
+        if (champ.val() == ""){
+            $errMsg.css("display", "block");
+            $nom.css({
+                borderColor : 'red',
+                color : 'red'
+            });
+            $prenom.css({
+                borderColor : 'red',
+                color: 'red'
+            })
+        }
+    }
+});
