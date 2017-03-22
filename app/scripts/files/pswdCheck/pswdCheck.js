8158e576dfdd5860b006561a74d5b9282f298c84
Etienne <etienne1296pro@gmail.com>
2017-03-22 14:50:29 +0100

    TODO + tuto en ligne

--- app/scripts/files/pswdCheck/pswdCheck.js
+++ app/scripts/files/pswdCheck/pswdCheck.js
@@ -4,6 +4,7 @@
  * CREATEUR : ETIENNE BLANC-COQUAND (etienne1296pro@gmail.com)
  * FONCTIONS : VERIFIER LA COMPLEXITE D'UN MOT DE PASSE ET SON EQUIVALENCE ENTRE LES DEUX CHAMPS
  */
+// TODO exemple de ce qu'il faut faire "http://www.webdesignerdepot.com/2012/01/password-strength-verification-with-jquery/"
 
 $(document).ready(function () {
     var $mdpBase = $("#nowPassword"),
